19e30904fa992229af93898eaabb4b1061a25026
reina <ReinaSHSL@users.noreply.github.com>
2019-06-13 16:16:26 -0700

    Test crop uses correct ctor

--- StardewValleyLib/Crops/TestCrop.cs
+++ StardewValleyLib/Crops/TestCrop.cs
@@ -12,20 +12,24 @@ namespace StardewValleyLib.Crops
         private new const string SeedName = "Test Flower Seeds";
         private new const CropType Type = CropType.FLOWER;
         private new const Season GrowthSeason = Season.SPRING;
-        private new int Phases = 2;
-        private new int RegrowthPhase = 2;
-        private new bool CanHarvestWithScythe = true;
-        private new bool IsTrellisCrop = true;
-        private new int MinHarvest = 1;
-        private new int MaxHarvest = 3;
-        private new int FarmSkillExp = 5;
-        private new double ExtraChance = 0.5;
-        private new int SeedPurchasePrice = 20;
-        private new Texture2D Texture = Game1.content.Load<Texture2D>("Resources/SDVLib/CreepFlower.png");
+        private new const int Phases = 2;
+        private new const int RegrowthPhase = 2;
+        private new const bool CanHarvestWithScythe = true;
+        private new const bool IsTrellisCrop = true;
+        private new const int MinHarvest = 1;
+        private new const int MaxHarvest = 3;
+        private new const int FarmSkillExp = 5;
+        private new const double ExtraChance = 0.5;
+        private new const int SeedPurchasePrice = 20;
+        private new static Texture2D Texture = Game1.content.Load<Texture2D>("Resources/SDVLib/CreepFlower.png");
 
 
-        public TestCrop()
+        public TestCrop() :
+        base(Name, Price, SeedName, Type, GrowthSeason, Phases, RegrowthPhase,
+             CanHarvestWithScythe, IsTrellisCrop, MinHarvest, MaxHarvest, FarmSkillExp,
+            ExtraChance, SeedPurchasePrice, Texture)
         {
+
         }
     }
 }
