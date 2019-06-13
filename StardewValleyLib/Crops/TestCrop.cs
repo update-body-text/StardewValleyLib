80688ecbdde3d8a959813c2115647676516f7e96
reina <ReinaSHSL@users.noreply.github.com>
2019-06-13 16:29:02 -0700

    Dont need to hide customcrop fields

--- StardewValleyLib/Crops/TestCrop.cs
+++ StardewValleyLib/Crops/TestCrop.cs
@@ -7,27 +7,27 @@ namespace StardewValleyLib.Crops
 {
     public class TestCrop : CustomCrop
     {
-        private new const string Name = "Test Flower";
-        private new const int Price = 2;
-        private new const string SeedName = "Test Flower Seeds";
-        private new const CropType Type = CropType.FLOWER;
-        private new const Season GrowthSeason = Season.SPRING;
-        private new const int Phases = 2;
-        private new const int RegrowthPhase = 2;
-        private new const bool CanHarvestWithScythe = true;
-        private new const bool IsTrellisCrop = true;
-        private new const int MinHarvest = 1;
-        private new const int MaxHarvest = 3;
-        private new const int FarmSkillExp = 5;
-        private new const double ExtraChance = 0.5;
-        private new const int SeedPurchasePrice = 20;
-        private new static Texture2D Texture = Game1.content.Load<Texture2D>("Resources/SDVLib/CreepFlower.png");
+        private const string NAME = "Test Flower";
+        private const int PRICE = 2;
+        private const string SEED_NAME = "Test Flower Seeds";
+        private const CropType TYPE = CropType.FLOWER;
+        private const Season GROWTH_SEASON = Season.SPRING;
+        private const int PHASES = 2;
+        private const int REGROWTH_PHASES = 2;
+        private const bool CAN_HARVEST_WITH_SCYTHE = true;
+        private const bool IS_TRELLIS_CROP = true;
+        private const int MIN_HARVEST = 1;
+        private const int MAX_HARVEST = 3;
+        private const int FARM_SKILL_EXP = 5;
+        private const double EXTRA_CHANCE = 0.5;
+        private const int SEED_PRICE = 20;
+        private static Texture2D TEXTURE = Game1.content.Load<Texture2D>("Resources/SDVLib/CreepFlower.png");
 
 
         public TestCrop() :
-        base(Name, Price, SeedName, Type, GrowthSeason, Phases, RegrowthPhase,
-             CanHarvestWithScythe, IsTrellisCrop, MinHarvest, MaxHarvest, FarmSkillExp,
-            ExtraChance, SeedPurchasePrice, Texture)
+        base(NAME, PRICE, SEED_NAME, TYPE, GROWTH_SEASON, PHASES, REGROWTH_PHASES,
+             CAN_HARVEST_WITH_SCYTHE, IS_TRELLIS_CROP,MIN_HARVEST,MAX_HARVEST,
+             FARM_SKILL_EXP, EXTRA_CHANCE, SEED_PRICE, TEXTURE)
         {
 
         }
