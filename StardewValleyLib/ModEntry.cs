47bf5770d79c3577e2349323d384f235b2aba09d
reina <ReinaSHSL@users.noreply.github.com>
2019-06-13 14:48:55 -0700

    Mod entry

---
+++ StardewValleyLib/ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardewValleyLib
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+
+    }
+}
