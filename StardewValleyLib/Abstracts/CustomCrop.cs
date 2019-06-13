f3517db7bca22abf4b624ee25db2f497d45a172e
reina <ReinaSHSL@users.noreply.github.com>
2019-06-13 15:01:37 -0700

    Custom crop data

---
+++ StardewValleyLib/Abstracts/CustomCrop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using StardewValley;
+
+namespace StardewValleyLib.Abstracts
+{
+
+    public class CustomCrop : Crop
+    {
+        private readonly String name;
+        private readonly int price;
+        private readonly String seedName;
+        private readonly CropType cropType;
+        private readonly Season season;
+        private readonly int phases;
+        private readonly int regrowthPhase;
+        private readonly bool canHarvestWithScythe;
+        private readonly bool isTrellisCrop;
+        private readonly int bonus;
+        private readonly int minHarvest;
+        private readonly int maxHarvest;
+        private readonly int farmSkillExp;
+        private readonly int extraChance;
+        private readonly int seedPurchasePrice;
+
+        public CustomCrop()
+        {
+        }
+
+        public enum CropType
+        {
+            FLOWER,
+            FRUIT,
+            VEGETABLE,
+            GEM,
+            FISH,
+            EGG,
+            MILK,
+            COOKING,
+            CRAFTING,
+            MINERAL,
+            MEAT,
+            METAL,
+            JUNK,
+            SYRUP,
+            MONSTER_LOOT,
+            ARTISAN_GOODS,
+            SEEDS
+        }
+
+        public enum Season
+        {
+            SPRING,
+            SUMMER,
+            FALL,
+            WINTER
+        }
+
+    }
+}
