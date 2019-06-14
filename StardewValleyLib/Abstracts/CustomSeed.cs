b4d290a1cac8b112ce13b0338ef23c030c70daf1
reina <ReinaSHSL@users.noreply.github.com>
2019-06-14 12:31:08 -0700

    woops

---
+++ StardewValleyLib/Abstracts/CustomSeed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using StardewValley;
+using StardewValley.Tools;
+using StardewValley.TerrainFeatures;
+using Microsoft.Xna.Framework;
+
+namespace StardewValleyLib.Abstracts
+{
+    public abstract class CustomSeed : StardewValley.Object
+    {
+        public CustomSeed()
+        {
+        }
+
+        public virtual void PlantSeed(Crop crop)
+        {
+            Vector2 tile = Game1.currentCursorTile;
+            tile = Utility.tileWithinRadiusOfPlayer((int)tile.X, (int)tile.Y, 1, Game1.player)
+                ? tile
+                : Game1.player.GetGrabTile();
+            if (!Game1.currentLocation.terrainFeatures.TryGetValue(tile, out TerrainFeature tf) || tf is HoeDirt)
+            {
+                HoeDirt dirt = (HoeDirt)tf;
+                dirt.crop = crop;
+            }
+        }
+    }
+}
---
+++ StardewValleyLib/Items/TestItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Tools;
+using StardewValleyLib.Crops;
+using StardewValleyLib.Abstracts;
+
+namespace StardewValleyLib.Items
+{
+    public class TestItem : CustomSeed
+    {
+        public TestItem()
+        {
+        
+        }
+
+        public override int sellToStorePrice()
+        {
+            return 0;
+        }
+
+
+        public override bool performToolAction(Tool t, GameLocation location)
+        {
+            PlantSeed(new TestCrop());
+            return true;
+        }
+
+      
+    }
+}
