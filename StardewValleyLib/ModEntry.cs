8ff40f23b62269ce7d50618ab1f27a262b99aab2
reina <ReinaSHSL@users.noreply.github.com>
2019-06-14 11:58:00 -0700

    ii dont know honeslty

--- StardewValleyLib/ModEntry.cs
+++ StardewValleyLib/ModEntry.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
+using StardewValleyLib.Items;
 using StardewValley;
 
 namespace StardewValleyLib
@@ -11,8 +12,22 @@ namespace StardewValleyLib
     {
         public override void Entry(IModHelper helper)
         {
-
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
         }
 
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+            this.Monitor.Log($"{e.Button}");
+            Vector2 tile = Game1.currentCursorTile;
+            tile = Utility.tileWithinRadiusOfPlayer((int)tile.X, (int)tile.Y, 1, Game1.player)
+                ? tile
+                : Game1.player.GetGrabTile();
+            this.Monitor.Log(tile.ToString());
+            Game1.getLocationFromName("Farm").dropObject(new TestItem(), tile, Game1.viewport, true, (Farmer)null);
+        }
+ 
     }
 }
